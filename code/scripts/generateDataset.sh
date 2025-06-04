835bb7b0c860b7eef5ec64d371fd71a52cff3fcd
Jesus <jesusico2001@gmail.com>
2025-06-04 17:57:11 +0200

    Clearingv2

--- code/scripts/generateDataset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-policy=$1
-numAgents=$2
-numSamples=$3
-
-python3 DatasetGenerator/Generator.py --policy $policy --numTrain 20000 --numVal 2000 --numTests 2000 --numSamples $numSamples  --seed 42 --numAgents $numAgents
-
--- code/scripts/tests/ICRA/ScalabilityTrain.sh
+++
@@ -1,6 +0,0 @@
-# Train
-bash scripts/trainModel.sh FS LEMURS 3 40000 50 4 fixed 800 fixed 1
-
-bash scripts/trainModel.sh TVS LEMURS 3 40000 50 4 fixed 800  fixed 1
-
-bash scripts/trainModel.sh Flocking LEMURS 3 40000 50 4 fixed 800  fixed 1
